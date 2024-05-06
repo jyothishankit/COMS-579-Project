44f6ce49fc5a5278f689d32fe3a1da0a4720070b
T sai ajay <tsaiajay@ajaytumu.student.iastate.edu>
2024-05-06 17:44:54 -0500

    Added changes for Funix

--- main.py
+++ main.py
@@ -8,90 +8,109 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import FlashrankRerank
 
-if len(sys.argv) < 2:
-  print("Atleast one of --pdf_file or --question must be sent. Usage: python main.py --pdf_file=a.pdf --question=\"What is the capital of USA?\"")
-  exit()
 
-pdf_file = None
-question = None
-for arg in sys.argv[1:]:
-  if arg.startswith("--pdf_file="):
-    pdf_file = arg.split("=")[1]
-  elif arg.startswith("--question="):
-    question = arg.split("=")[1]
-  else:
-    print(f"Invalid argument: {arg}")
-    exit()
+def upload(pdf_file_1,pdf_file_2,pdf_file_3,question):
 
-if not pdf_file or not question:
-  print("Error: Missing required arguments!")
-  exit()
+# if len(sys.argv) < 2:
+#   print("Atleast one of --pdf_file or --question must be sent. Usage: python main.py --pdf_file=a.pdf --question=\"What is the capital of USA?\"")
+#   exit()
 
-if(len(sys.argv)<2):
-    print("No pdf document passed")
-    exit(0)
+# pdf_file = None
+# question = None
+# for arg in sys.argv[1:]:
+#   if arg.startswith("--pdf_file="):
+#     pdf_file = arg.split("=")[1]
+#   elif arg.startswith("--question="):
+#     question = arg.split("=")[1]
+#   else:
+#     print(f"Invalid argument: {arg}")
+#     exit()
 
-loader = PyPDFLoader(pdf_file)
-pages = loader.load_and_split()
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200, add_start_index=True
-)
-split_docs = text_splitter.split_documents(pages)
+# if not pdf_file or not question:
+#   print("Error: Missing required arguments!")
+#   exit()
 
-#Can print initial file contents
-#print(split_docs[0].page_content)
-embedding_model_name = "google-bert/bert-base-uncased"
+# if(len(sys.argv)<2):
+#     print("No pdf document passed")
+#     exit(0)
 
-WEAVIATE_URL="http://localhost:8080"
-# class_object_document = {
-#     "class": "Document",
-#     "vectorizer": "text2vec-huggingface",
-#     "moduleConfig": {
-#         "text2vec-huggingface": {
-#             "vectorizeClassName": True
-#         }
-#     }
-# }
+    loader = PyPDFLoader(pdf_file_1)
+    pages = loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200, add_start_index=True
+    )
+    split_docs_1 = text_splitter.split_documents(pages)
+    print(str(len(split_docs_1))+" is the length of split docs for pdf 1")
 
-client = weaviate.Client(
-    url=WEAVIATE_URL
-)
+    loader = PyPDFLoader(pdf_file_2)
+    pages = loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200, add_start_index=True
+    )
+    split_docs_2 = text_splitter.split_documents(pages)
+    print(str(len(split_docs_2))+" is the length of split docs for pdf 2")
 
-#client.schema.create_class(class_object_document)
+    loader = PyPDFLoader(pdf_file_3)
+    pages = loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200, add_start_index=True
+    )
+    split_docs_3 = text_splitter.split_documents(pages)
 
-embeddings = HuggingFaceEmbeddings(
-  model_name=embedding_model_name
-)
-#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
+    #Can print initial file contents
+    #print(split_docs[0].page_content)
+    
+    #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
 
-#split_docs=split_docs[:5]
-print(str(len(split_docs))+" is the length of split docs")
-vector_store = Weaviate.from_documents(documents=split_docs, embedding=embeddings, client=client)
+    #split_docs=split_docs[:5]
+    print(str(len(split_docs_3))+" is the length of split docs for pdf 3")
 
-print("Reached here after storing documents in vector store")
-print("\n\n")
-retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 5})
-print("Question is "+question)
-retrieved_docs = retriever.invoke(question)
-print("\n\n")
-print("Full output of retriever")
-print(retrieved_docs)
-print("\n")
-print("\n")
-print("Answer to the question:\n")
-print(retrieved_docs[0].page_content)
 
-print("\n\n")
-print("Using reranking for fetching based on context")
-retriever_contextual_with_reranking = ContextualCompressionRetriever(base_retriever=retriever,base_compressor=FlashrankRerank(),query=question, search_kwargs={"k": 5})
+    embedding_model_name = "google-bert/bert-base-uncased"
 
-reranked_docs = retriever_contextual_with_reranking.get_relevant_documents(question)
+    WEAVIATE_URL="http://localhost:8080"
 
-print("\n\n")
-print("Output of reranked documents with relevance scores")
-print(reranked_docs)
+    client = weaviate.Client(
+        url=WEAVIATE_URL
+    )
 
-print("\n\n")
-print("Top answer of reranked vectors")
-print(reranked_docs[0].page_content)
+    #client.schema.create_class(class_object_document)
 
+    embeddings = HuggingFaceEmbeddings(
+    model_name=embedding_model_name
+    )
+    split_docs = split_docs_1 + split_docs_2 + split_docs_3
+    vector_store = Weaviate.from_documents(documents=split_docs, embedding=embeddings, client=client)
+
+    print("Reached here after storing documents in vector store")
+    print("\n\n")
+    #return "Stored documents in vector store"
+
+
+#def answer_question(question):
+    retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 5})
+    print("Question is "+question)
+    retrieved_docs = retriever.invoke(question)
+    print("\n\n")
+    print("Full output of retriever")
+    print(retrieved_docs)
+    print("\n")
+    print("\n")
+    print("Answer to the question:\n")
+    print(retrieved_docs[0].page_content)
+
+    print("\n\n")
+    print("Using reranking for fetching based on context")
+    retriever_contextual_with_reranking = ContextualCompressionRetriever(base_retriever=retriever,base_compressor=FlashrankRerank(),query=question, search_kwargs={"k": 5})
+
+    reranked_docs = retriever_contextual_with_reranking.get_relevant_documents(question)
+
+    print("\n\n")
+    print("Output of reranked documents with relevance scores")
+    print(reranked_docs)
+
+    print("\n\n")
+    print("Top answer of reranked vectors")
+    print(reranked_docs[0].page_content)
+    final_answer = reranked_docs[0].page_content
+    return final_answer
---
+++ upload.py
@@ -0,0 +1,4 @@
+import main
+
+def upload(pdf_file_name_1: str, pdf_file_name_2: str, pdf_file_name_3: str, question: str) -> str:
+    return main.upload(pdf_file_1=pdf_file_name_1,pdf_file_2=pdf_file_name_2, pdf_file_3=pdf_file_name_3,question=question)
