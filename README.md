0c733a8459811e78f4c4add4bcda1ca2f7ab4b59
T sai ajay <tsaiajay@ajaytumu.student.iastate.edu>
2024-05-06 17:50:23 -0500

    Added changes for removing comments, hello.py and modified README.md

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # COMS-579-Project
 RAG
 
-Python version used: 3.9.0
+Python version used: 3.10.0
 
 Steps to run:
 1. Run Docker Desktop/Docker daemon process.
 2. Open 2 terminals.
 3. First terminal: Run "docker compose up -d"
 4. Second terminal, follow the below steps.
-5. python version of 3.9.0 to be used:
+5. python version of 3.10.0 to be used:
 6.  python --version
-Python 3.9.0
+Python 3.10.0
 7. Run "python -m venv ." in the root level of project(containing requirements_local.txt)
 8. Activate virtual env, For Windows use: "Scripts\activate", For MacOS/Linux use "source bin/activate" 
 9. "pip install -r requirements_local.txt".
-10. Run "python main.py --pdf_file=genemutation.pdf --question="What is the relation between hypermutable brains and age?"". (genemutation.pdf is the file name)
+10. Run "funix upload.py".
 11. !!Caution: Code takes more than 7mins to show output.
 
 Video link for RAG demo for indexing, spliting, fetching nearby vector:
--- main.py
+++ main.py
@@ -10,30 +10,6 @@ from langchain.retrievers.document_compressors import FlashrankRerank
 
 
 def upload(pdf_file_1,pdf_file_2,pdf_file_3,question):
-
-# if len(sys.argv) < 2:
-#   print("Atleast one of --pdf_file or --question must be sent. Usage: python main.py --pdf_file=a.pdf --question=\"What is the capital of USA?\"")
-#   exit()
-
-# pdf_file = None
-# question = None
-# for arg in sys.argv[1:]:
-#   if arg.startswith("--pdf_file="):
-#     pdf_file = arg.split("=")[1]
-#   elif arg.startswith("--question="):
-#     question = arg.split("=")[1]
-#   else:
-#     print(f"Invalid argument: {arg}")
-#     exit()
-
-# if not pdf_file or not question:
-#   print("Error: Missing required arguments!")
-#   exit()
-
-# if(len(sys.argv)<2):
-#     print("No pdf document passed")
-#     exit(0)
-
     loader = PyPDFLoader(pdf_file_1)
     pages = loader.load_and_split()
     text_splitter = RecursiveCharacterTextSplitter(
@@ -56,13 +32,6 @@ def upload(pdf_file_1,pdf_file_2,pdf_file_3,question):
         chunk_size=1000, chunk_overlap=200, add_start_index=True
     )
     split_docs_3 = text_splitter.split_documents(pages)
-
-    #Can print initial file contents
-    #print(split_docs[0].page_content)
-    
-    #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
-
-    #split_docs=split_docs[:5]
     print(str(len(split_docs_3))+" is the length of split docs for pdf 3")
 
 
@@ -74,8 +43,6 @@ def upload(pdf_file_1,pdf_file_2,pdf_file_3,question):
         url=WEAVIATE_URL
     )
 
-    #client.schema.create_class(class_object_document)
-
     embeddings = HuggingFaceEmbeddings(
     model_name=embedding_model_name
     )
@@ -84,10 +51,7 @@ def upload(pdf_file_1,pdf_file_2,pdf_file_3,question):
 
     print("Reached here after storing documents in vector store")
     print("\n\n")
-    #return "Stored documents in vector store"
-
-
-#def answer_question(question):
+    
     retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 5})
     print("Question is "+question)
     retrieved_docs = retriever.invoke(question)
